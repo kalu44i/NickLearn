6b5762dafad02e6328efbdab23ab2cda9053f11e
Nick <mykola.veremeichyk@gmail.com>
2014-12-08 19:59:58 +0200

    Has added Person descriptors

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,51 @@
+import java.util.Date;
+
 
 public class Person {
-
+	private String name;
+	private String surname;
+	private int age;
+	private String citizenShip;
+	private String gender;
+	private Date birthday;
+	private String maritalStatus;
+	private int height;
+	private int weight;
+	
+	String getName() {
+		return name;
+	}
+	
+	String getSurname() {
+		return surname;
+	}
+	
+	int getAge() {
+		return age;
+	}
+	
+	String getCitizenShip() {
+		return citizenShip;
+	}
+	
+	String getGender() {
+		return gender;
+	}
+	
+	Date getBirthday() {
+		return birthday;
+	}
+	
+	String getMaritalStatus() {
+		return maritalStatus;
+	}
+	
+	int getHeight() {
+		return height;
+	}
+	
+	int hetWeight() {
+		return weight;
+	}
+	
 }
