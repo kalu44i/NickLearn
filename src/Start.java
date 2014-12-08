0e7268d9f8c8549aa82b22b1c27ab1fb5962206e
Nick <mykola.veremeichyk@gmail.com>
2014-12-08 20:53:59 +0200

    Add new person logic

--- src/Start.java
+++ src/Start.java
@@ -1,8 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.Buffer;
+
 public class Start {
 
-	public static void main(String[] args) {
-		Person p = new Person("petjka", "pukin", "man");
-		System.out.println(p.getName() + " " +p.getGender() + " " + p.getCitizenShip());
+	public static void main(String[] args) throws IOException {
+	
+		System.out.println("Add new person to your program!");
+		System.out.println("Your name:");
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String name = in.readLine();
+		System.out.println("Your Surname:");
+		String surname = in.readLine();
+		int age = Integer.parseInt(in.readLine());
+		String citizenShip = in.readLine();
+		String gender = in.readLine();
+		String maritalStatus = in.readLine();
+		int height = Integer.parseInt(in.readLine());
+		int weight = Integer.parseInt(in.readLine());
+
+		Person p = new Person(name, surname, age, citizenShip, gender, maritalStatus, height, weight);
 	}
 
 }
