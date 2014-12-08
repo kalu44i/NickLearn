812b8a67bd9a51d592e911df3330c3b7e06909bd
Nick <mykola.veremeichyk@gmail.com>
2014-12-08 20:36:39 +0200

    Add constructors to the Person class

--- src/Person.java
+++ src/Person.java
@@ -1,51 +1,75 @@
 import java.util.Date;
 
-
 public class Person {
 	private String name;
 	private String surname;
 	private int age;
 	private String citizenShip;
 	private String gender;
-	private Date birthday;
+	// private Date birthday;
 	private String maritalStatus;
 	private int height;
 	private int weight;
-	
+
+	public Person(String name, String surname, int age, String citizenShip,
+			String gender, String maritalStatus, int height, int weight) {
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
+		this.citizenShip = citizenShip;
+		this.gender = gender;
+		// this.birthday = birthday;
+		this.maritalStatus = maritalStatus;
+		this.height = height;
+		this.weight = weight;
+	}
+
+	public Person(String name, String surname, String gender) {
+		this.name = name;
+		this.surname = surname;
+		this.age = 18;
+		this.citizenShip = "citizenShip";
+		this.gender = gender;
+		// this.birthday = birthday;
+		this.maritalStatus = "maritalStatus";
+		this.height = 0;
+		this.weight = 0;
+	}
+
 	String getName() {
 		return name;
 	}
-	
+
 	String getSurname() {
 		return surname;
 	}
-	
+
 	int getAge() {
 		return age;
 	}
-	
+
 	String getCitizenShip() {
 		return citizenShip;
 	}
-	
+
 	String getGender() {
 		return gender;
 	}
-	
-	Date getBirthday() {
-		return birthday;
-	}
-	
+
+	// Date getBirthday() {
+	// return birthday;
+	// }
+
 	String getMaritalStatus() {
 		return maritalStatus;
 	}
-	
+
 	int getHeight() {
 		return height;
 	}
-	
+
 	int hetWeight() {
 		return weight;
 	}
-	
+
 }
--- src/Start.java
+++ src/Start.java
@@ -1,8 +1,8 @@
 public class Start {
 
 	public static void main(String[] args) {
-		Person p = new Person();
-
+		Person p = new Person("petjka", "pukin", "man");
+		System.out.println(p.getName() + " " +p.getGender() + " " + p.getCitizenShip());
 	}
 
 }
